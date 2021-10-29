94d0adc6c5147ab4d99fe62df97246d04ef1f7d8
soyikimm <flamingo7763@gmail.com>
2021-10-29 11:22:11 +0900

    Test Rules

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>TEST APP♥</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ //자동완성 기능이있어 편리함
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 28,
+    color: "blue",
   },
 });
+ 
