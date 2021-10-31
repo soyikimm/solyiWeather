45355bfdce4b4c30082d61e087839805c3abbf11
soyikimm <flamingo7763@gmail.com>
2021-10-31 13:51:06 +0900

    Scroll

--- App.js
+++ App.js
@@ -1,26 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, Dimensions, View, ScrollView } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
-    <View style={{flex:1, flexDirection:"row"}}>
-      <View style={{ flex: 1, backgroundColor:"tomato"}}></View>
-      <View style={{ flex: 5, backgroundColor:"teal"}}></View>
-      <View style={{ flex: 1, backgroundColor:"orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Pyeongteak</Text>
+      </View>
+      <ScrollView 
+      horizontal 
+      pagingEnabled //옆으로 넘겼을때 딱딱 떨어지게 해줌
+      showsHorizontalScrollIndicator="false" //스크롤인디케이터 사라지게함(밑에 스크롤바)
+      contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>50˚</Text>
+          <Text style={styles.description}>Hell</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>50˚</Text>
+          <Text style={styles.description}>Hell</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>50˚</Text>
+          <Text style={styles.description}>Hell</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>50˚</Text>
+          <Text style={styles.description}>Hell</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>50˚</Text>
+          <Text style={styles.description}>Hell</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({ //자동완성 기능이있어 편리함
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+const styles = StyleSheet.create ({
+  container:{
+    flex: 1, 
+    backgroundColor:"tomato",
   },
-  text: {
-    fontSize: 28,
-    color: "blue",
+  city: {
+    flex :1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  cityName: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  weather: {
+  },
+  day: {
+    width : SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 160,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
